97970f6a5acc5f36d527a6b90aff3dcc97ce331e
Sam Mcloughlin <24403822@studentmail.ul.ie>
2024-11-18 15:49:12 +0000

    Precidence

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,27 @@
 //TODO: Make abstract
 public class Calculator {
+    public Calculator(){
+
+    }
+
+    // multiplication and division has less precidence than addition and subtraction
+    protected int precidence(String op1){
+        int precidence;
+        try{
+            if (op1.equals("*") || op1.equals("/")){
+                return 2;
+            } else if (op1.equals("+") || op1.equals("-")) {
+                return 1;
+            }
+        } catch (Exception e){
+            System.out.println("Incorrect value for op1 (" + op1 + ") " + e);
+        }
+
+        return -1;
+    }
+
+    //if the first operator has more precidence or is the same than the second operator, return true
+    protected boolean hasPrecidence(String op1, String op2){
+        return (precidence(op1) >= precidence(op2));
+    }
 }
