d2f835f02c0c1217795090816084c2b7dc853a43
Yasmin Woodlock <yasminwoodlock@gmail.com>
2024-12-16 10:31:50 +0000

    added loop to see what will be calculated

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ Title?:
   \_____\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|
  */
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     //some data to test with
     private static ArrayList<String> buildTest(){
@@ -36,6 +39,8 @@ public class Main {
 //        System.out.println("Test1 Expected Output: " + testOutput + "\nCurrent Output: " + ParseArgs.parseArgs(test));
 //    }
     public static void main(String[] args) {
+        boolean running = true;
+
         System.out.printf("   _____      _            _       _\n" +
                 "  / ____|    | |          | |     | |\n" +
                 " | |     __ _| | ___ _   _| | __ _| |_ ___  _ __\n" +
@@ -47,6 +52,33 @@ public class Main {
             System.out.println(postThing.calculate());
         } catch (ArithmeticException e){
             System.out.println("Something went wrong: " + e);
+        }
+
+        System.out.println(args.length==0);
+
+        while(running){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Would you like to evaluate in:\n1) infix(normal)\n2) prefix\n3) postfix?\nPress 1, 2 or 3. Press x to exit: ");
+
+            try {
+                String input = scanner.nextLine().strip();
+
+                if (input.equals("x")) {
+                    System.out.println("Thank you for using Stacky!");
+                    running = false;
+                } else if (input.equals("1")) {
+                    InfixCalculator infix = new InfixCalculator(buildTest());
+                    infix.calculate();
+                } else if (input.equals("2")){
+                    PrefixCalculator prefix = new PrefixCalculator(buildTest());
+                    prefix.calculate();
+                } else if (input.equals("3")){
+                    PostfixCalculator postfix = new PostfixCalculator(buildTest());
+                    postfix.calculate();
+                }
+            }catch (InputMismatchException | ArithmeticException e){
+                System.out.println("Something went wrong: " + e);
+            }
         }
 //        stringTest1();
 //        stringTest2();
