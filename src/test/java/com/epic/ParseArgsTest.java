a21a2c282d980cb34794b81f63c7ce79360055b1
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-12-16 16:35:49 +0000

    removed target folder | updated pom compile dependencies | gitignore updated

--- src/test/java/com/epic/ParseArgsTest.java
+++ src/test/java/com/epic/ParseArgsTest.java
@@ -1,48 +1,31 @@
 package com.epic;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class ParseArgsTest {
     @Test
-    void testEquality() {
-        int expected = 5;
-        int actual = 2 + 3;
-        assertEquals(expected, actual, "The values should be equal");
+    void testExpressionSplitting() {
+        String[] test = {"(1.9 - 5) * -5^3"};
+        List<String> expected = Arrays.asList("(", "1.9", "-", ")", "*", "-5", "^", "3");
+        List<String> actual = ParseArgs.parseArgs(test);
+        assertEquals(expected, actual);
     }
-
-    @Test
-    void testTrueCondition() {
-        boolean condition = 5 > 2;
-        assertTrue(condition, "The condition should be true");
-    }
-
-    @Test
-    void testFalseCondition() {
-        boolean condition = 2 > 5;
-        assertFalse(condition, "The condition should be false");
-    }
-
-    @Test
-    void testNotNull() {
-        String str = "JUnit";
-        assertNotNull(str, "The object should not be null");
-    }
-
-    @Test
-    void testArrayEquality() {
-        int[] expectedArray = {1, 2, 3};
-        int[] actualArray = {1, 2, 3};
-        assertArrayEquals(expectedArray, actualArray, "The arrays should be equal");
-    }
-
-    @Test
-    void testThrowsException() {
-        Exception exception = assertThrows(ArithmeticException.class, () -> {
-            int result = 1 / 0;
-        });
-        assertEquals("/ by zero", exception.getMessage(), "Exception message should match");
-    }
-
 }
+
+//    private static void stringTest2(){
+//        String[] test = {"(1.9 - 5 * -5^3"};
+//        String testOutput = "new exception, incorrect amount of brackets";
+//        System.out.println("Test1 Expected Output: " + testOutput + "\nCurrent Output: " + ParseArgs.parseArgs(test));
+//    }
+//
+//    private static void stringTest3(){
+//        String[] test = {"(1.9 - * 5) * + 5^3"};
+//        String testOutput = "new exception, + and * shouldnt be together";
+//        System.out.println("Test1 Expected Output: " + testOutput + "\nCurrent Output: " + ParseArgs.parseArgs(test));
+//    }
