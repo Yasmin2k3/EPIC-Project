cc38fde3311d2746fc7263c82f76122ec7a276cd
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-12-18 13:12:04 +0000

    testing updated with anotehr assertation

--- src/test/java/com/epic/ParseArgsTest.java
+++ src/test/java/com/epic/ParseArgsTest.java
@@ -9,11 +9,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class ParseArgsTest {
+    Parser parser;
+
+    ParseArgsTest() {
+        this.parser = new Parser();
+    }
+
     @Test
     void testExpressionSplitting() {
         String input = "(1.9 - 5) * -5^3";
         List<String> expected = Arrays.asList("(", "1.9", "-", "5", ")", "*", "-5", "^", "3");
-        Parser parser = new Parser();
         List<String> actual = parser.parse(input);
         assertEquals(expected, actual);
     }
@@ -21,7 +26,6 @@ class ParseArgsTest {
     @Test
     void testInvalidExpression() {
         String input = "(1.9 - * 5) * + 5^3";
-        Parser parser = new Parser();
         assertThrows(InvalidExpressionException.class, () -> parser.parse(input));
     }
 
@@ -29,8 +33,13 @@ class ParseArgsTest {
     void testInvalidExpression2() {
         String input = "12 / (2 + 1 )";
         List<String> expected = Arrays.asList("(", "12", "^", "2", "+", "1", "^");
-        Parser parser = new Parser();
         List<String> actual = parser.parse(input);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testUnmatchedParentheses() {
+        String input = "(5+3";
+        assertThrows(InvalidExpressionException.class, () -> parser.parse(input));
     }
 }
