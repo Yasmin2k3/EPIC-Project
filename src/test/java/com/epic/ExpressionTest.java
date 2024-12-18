78b16e1b8093ddb34b849ac2c023eb300cd2aa98
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2024-12-18 19:57:13 +0000

    all 23 tests passing

--- src/test/java/com/epic/ExpressionTest.java
+++ src/test/java/com/epic/ExpressionTest.java
@@ -1,4 +1,11 @@
 package com.epic;
 
+import org.junit.jupiter.api.Test;
+
 public class ExpressionTest {
+    @Test
+    public void test() {
+        Expression expression = new Expression("(1.9 - 5) * -5^3 /0");
+    }
+
 }
