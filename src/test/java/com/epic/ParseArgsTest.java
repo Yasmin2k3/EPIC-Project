1679b3836e20466bafbd1d3b8ce8ab1470063021
Yasmin Woodlock <119609274+Yasmin2k3@users.noreply.github.com>
2024-12-18 22:32:23 +0000

    added parseArgsTest cases

--- src/test/java/com/epic/ParseArgsTest.java
+++ src/test/java/com/epic/ParseArgsTest.java
@@ -5,16 +5,100 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class ParseArgsTest
 {
     @Test
-    void testExpressionSplitting() {
+    void testIsValidExpression() {
         String input = "(1.9 - 5) * -5^3";
+        Parser parser = new Parser();
         List<String> expected = Arrays.asList("(", "1.9", "-", "5", ")", "*", "-5", "^", "3");
         List<String> actual = parser.parse(input);
         assertEquals(expected, actual);
+    }
+    @Test
+    void hasInvalidExponentialOperatorNoBase()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("^3"));
+    }
+    @Test
+    void hasInvalidExponentialOperatorNoExponent()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("3^"));
+    }
+    @Test
+    public void testHasUnmatchedParenthesis()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("("));
+    }
+
+    @Test
+    void testHasSequentialDecimalPoints()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse(".."));
+    }
+
+    @Test
+    void testHasEmptyParenthesis()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("()"));
+    }
+
+    @Test
+    void testHasInvalidVocabulary()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("a"));
+    }
+
+    @Test
+    void testHasIllegalOperatorSequenceMinusMultiply()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("-*"));
+    }
+
+    @Test
+    void testHasIllegalOperatorSequenceDivideMultiply()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("/*"));
+    }
+
+    @Test
+    void testHasIllegalOperatorSequenceMultiplyDivide()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("*/"));
+    }
+
+    @Test
+    void testHasIllegalOperatorSequencePlusMultiply()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("+*"));
+    }
+
+    @Test
+    void testHasIllegalOperatorSequenceMinusDivide()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("-/"));
+    }
+
+
+    @Test
+    void testHasOperandWithUnmatchedValue()
+    {
+        Parser parser = new Parser();
+        assertThrows(InvalidExpressionException.class, ()-> parser.parse("22-"));
     }
 }
